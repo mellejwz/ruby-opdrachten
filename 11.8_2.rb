97882d63df2c6cd2298767e8db2679ea007899af
Melle Zwerver <mellezwerver@outlook.com>
2015-03-16 22:28:02 +0100

    finished playlist program, forgot to shuffle it

--- 11.8_2.rb
+++ 11.8_2.rb
@@ -1,11 +1,23 @@
 Dir.chdir 'C:/Users/melle/Music'
 
-filename = 'all_songs.m3u'
+@filename = 'all_songs.m3u'
 
 songs = Dir['**/*.mp3']
 
-File.open filename, 'w' do |playlist|
-	songs.each do |song| 
-		playlist.write "#{song}\n"
+def shuffle_songs sorted, shuffled
+	random = rand(sorted.length)
+
+	shuffled.push sorted[random]
+	sorted.delete(sorted[random])
+
+	if sorted.length==0
+		File.open @filename, 'w' do |playlist|
+			shuffled.each do |song| 
+				playlist.write "#{song}\n"
+			end
+		end
+	else shuffle_songs sorted, shuffled
 	end
 end
+
+shuffle_songs songs, []
